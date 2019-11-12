f340f8f5887218c279a4eed3df41ba7b2c1750e2
Carl Anderson <carl.anderson@microsoft.com>
2019-11-12 13:25:31 -0600

    tabs to spaces

--- login.js
+++ login.js
@@ -6,37 +6,37 @@ var path = require('path');
 
 var app = express();
 app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
 }));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/login.html'));
+  response.sendFile(path.join(__dirname + '/login.html'));
 });
 
 app.post('/auth', function(request, response) {
-	var username = request.body.username;
-	var password = request.body.password;
-	if (username && password) {
+  var username = request.body.username;
+  var password = request.body.password;
+  if (username && password) {
     request.session.loggedin = true;
     request.session.username = username;
     response.redirect('/home');
-	} else {
-		response.send('Please enter Username and Password!');
-	}
+  } else {
+    response.send('Please enter Username and Password!');
+  }
   response.end();
 });
 
 app.get('/home', function(request, response) {
-	if (request.session.loggedin) {
-		response.send('Welcome back, Dr. ' + request.session.username + '!');
-	} else {
-		response.send('Please login to view this page!');
-	}
-	response.end();
+  if (request.session.loggedin) {
+    response.send('Welcome back, Dr. ' + request.session.username + '!');
+  } else {
+    response.send('Please login to view this page!');
+  }
+  response.end();
 });
 
 app.listen(3000);
