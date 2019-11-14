46ae256e6804649765ff1f6fb66cad8d6fea7097
Carl Anderson <carl.anderson@microsoft.com>
2019-11-14 14:04:56 -0600

    feat: implements basic guideline consultation

--- index.js
+++ index.js
@@ -4,6 +4,63 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
 const port = process.env.PORT || '3000';
+const login = `<a href="http://localhost:${port}/">login</a>`;
+
+const CPT = {
+  _FHIR_CODING_SYSTEM: 'http://www.ama-assn.org/go/cpt',
+  CARDIAC_MRI: '75561',
+  CT_HEAD_NO_CONTRAST: '70450',
+  CTA_WITH_CONTRAST: '71275',
+  LUMBAR_SPINE_CT: '72133',
+  MRA_HEAD: '70544',
+};
+
+const SNOMED = {
+  _FHIR_CODING_SYSTEM: 'http://snomed.info/sct',
+  CONGENITAL_HEART_DISEASE: '13213009',
+  HEADACHE: '25064002',
+  LOW_BACK_PAIN: '279039007',
+  OPTIC_DISC_EDEMA: '423341008',
+  TOOTHACHE: '27355003'
+};
+
+class Reasons {
+  static covers(subset, set) {
+    if (subset.size > set.size) {
+      return false;
+    }
+    for (const member of subset) {
+      if (!set.has(member)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  constructor(appropriate, notAppropriate) {
+    this.appropriate = appropriate.map(x => new Set(x));
+    this.notAppropriate = notAppropriate.map(x => new Set(x));
+  }
+
+  getRating(reasons) {
+    if (this.appropriate.filter(s => Reasons.covers(s, reasons)).length) {
+      return 'appropriate';
+    }
+    if (this.notAppropriate.filter(s => Reasons.covers(s, reasons)).length) {
+      return 'not-appropriate';
+    }
+    return 'no-guidelines-apply';
+  }
+}
+
+const cptReasons = {
+  'no-procedures-for': new Reasons([[SNOMED.TOOTHACHE]], []),
+  [CPT.CT_HEAD_NO_CONTRAST]: new Reasons([[SNOMED.HEADACHE, SNOMED.OPTIC_DISC_EDEMA]], []),
+  [CPT.MRA_HEAD]: new Reasons([], []),
+  [CPT.CTA_WITH_CONTRAST]: new Reasons([], [[SNOMED.CONGENITAL_HEART_DISEASE]]),
+  [CPT.LUMBAR_SPINE_CT]: new Reasons([], [[SNOMED.LOW_BACK_PAIN]]),
+  [CPT.CARDIAC_MRI]: new Reasons([[SNOMED.CONGENITAL_HEART_DISEASE]], []),
+};
 
 const app = express();
 app.use(session({
@@ -26,16 +83,17 @@ app.get('/consult', function(request, response) {
   if (request.session.loggedin) {
     response.sendFile(path.join(__dirname + '/consult.html'));
   } else {
-    const login = `<a href="http://localhost:${port}/">login</a>`;
     response.send(`Please ${login} to view this page!`).end();
   }
 });
 
 app.post('/evaluate', function(request, response) {
   if (!request.session.loggedin) {
-    response.status(404).send('You must be logged in to access this.');
+    response.status(404).send(`You must ${login} to access this.`);
   } else {
-    response.status(200).send('Evaluating demographics ' + JSON.stringify(request.body));
+    const reasons = cptReasons[request.body.procedure];
+    const rating = reasons.getRating(new Set([request.body.indication]));
+    response.status(200).send(rating);
   }
   response.end();
 });
