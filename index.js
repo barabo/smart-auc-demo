4388ecf71b5caf1b0bdd94aa6a7061ba197432c1
Carl Anderson <carl.anderson@microsoft.com>
2019-11-14 12:30:58 -0600

    feat: added the ability to enter age and gender demographics

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const express = require('express');
+// See: https://codeshack.io/basic-login-system-nodejs-express-mysql/
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-const app = express();
-const port = process.env.PORT || '8123';
+var app = express();
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!');
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname + '/login.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Listening to requests on http://localhost:${port}`);
+app.post('/auth', function(request, response) {
+  var username = request.body.username;
+  var password = request.body.password;
+  if (username && password) {
+    request.session.loggedin = true;
+    request.session.username = username;
+    response.redirect('/home');
+  } else {
+    response.send('Please enter Username and Password!');
+  }
+  response.end();
 });
+
+app.get('/home', function(request, response) {
+  if (request.session.loggedin) {
+    response.send('Welcome back, Dr. ' + request.session.username + '!');
+  } else {
+    response.send('Please login to view this page!');
+  }
+  response.end();
+});
+
+app.listen(3000);
